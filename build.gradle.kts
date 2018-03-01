6f7d7b8412f6f408694845f354de0a7ef5bae1e6
Ben Chatelain <ben@octop.ad>
2018-02-28 17:50:42 -0700

    Add listKeychains task

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,30 @@
 
 
 /* -------------------------------------------------------------------------- */
+// Imports must be before buildscript & plugins
+/* -------------------------------------------------------------------------- */
+
+import at.phatbl.shellexec.ShellExec
+
+/* -------------------------------------------------------------------------- */
 // Build Script
 /* -------------------------------------------------------------------------- */
+
+/*
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("at.phatbl:shellexec:+")
+    }
+}
+*/
 
 plugins {
     id("at.phatbl.shellexec") version "1.1.0"
 }
+
 
 val removeBatchFile by tasks.creating(Delete::class) { delete("gradlew.bat") }
 
@@ -20,4 +38,20 @@ tasks {
         distributionType = Wrapper.DistributionType.ALL
         finalizedBy(removeBatchFile)
     }
+}
+
+/* -------------------------------------------------------------------------- */
+// Ad Hoc Tasks
+/* -------------------------------------------------------------------------- */
+
+/*
+list-keychains [-h] [-d user|system|common|dynamic] [-s [keychain...]]
+Display or manipulate the keychain search list.
+
+-d user|system|common|dynamic
+        Use the specified preference domain.
+-s       Set the search list to the specified keychains.
+*/
+val listKeychains by tasks.creating(ShellExec::class) {
+    command = "security list-keychains -d user"
 }
