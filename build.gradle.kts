11d4b35a4e8c7a337ec71809eb91b4cf2de10abe
Ben Chatelain <ben@octop.ad>
2018-03-01 09:10:01 -0700

    Add findIdentity task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
  * Security
  */
 
-
 /* -------------------------------------------------------------------------- */
 // Imports must be before buildscript & plugins
 /* -------------------------------------------------------------------------- */
@@ -29,7 +28,6 @@ plugins {
     id("at.phatbl.shellexec") version "1.1.0"
 }
 
-
 val removeBatchFile by tasks.creating(Delete::class) { delete("gradlew.bat") }
 
 tasks {
@@ -54,4 +52,31 @@ Display or manipulate the keychain search list.
 */
 val listKeychains by tasks.creating(ShellExec::class) {
     command = "security list-keychains -d user"
+}
+
+/*`
+find-identity [-h] [-p policy] [-s string] [-v] [keychain...]
+Find an identity (certificate + private key) satisfying a given policy. If no policy arguments are provided, the X.509 basic policy is assumed. If no keychain arguments are provided, the default search list is used.
+
+Options:
+-p policy       Specify policy to evaluate (multiple -p options are allowed). Supported policies: basic, ssl-client, ssl-server, smime, eap, ipsec, ichat, codesigning, sys-default, sys-kerberos-kdc
+-s string       Specify optional policy-specific string (e.g. a DNS hostname for SSL, or RFC822 email address for S/MIME)
+-v              Show valid identities only (default is to show all identities)
+
+Examples
+
+security> find-identity -v -p ssl-client
+Display valid identities that can be used for SSL client authentication
+
+security> find-identity -p ssl-server -s www.domain.com
+        Display identities for a SSL server running on the host 'www.domain.com'
+
+security> find-identity -p smime -s user@domain.com
+Display identities that can be used to sign a message from 'user@domain.com'
+*/
+val findIdentity by tasks.creating(ShellExec::class) {
+    command = "security find-identity -p codesigning"
+/*    preExec() {
+        command += "security.keychain"
+    }*/
 }
